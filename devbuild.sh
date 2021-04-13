fe2a46d695c0df177c82c6f45299c28ae797375e
Daniel Rosen <daniel.rosen@noaa.gov>
2021-04-13 03:34:17 +0000

    Add INCLUDE_COMPONENTS to ufs_weather_model ExternalProject 1. Update src/ufs_weather_model revision 2. Add --components option to devbuild.sh 3. Append components to CMAKE_ARGS in src/CMakeLists.txt

--- devbuild.sh
+++ devbuild.sh
@@ -1,41 +1,92 @@
 #!/bin/bash
+
+usage () {
+  printf "Usage: $0 PLATFORM [OPTIONS]...\n"
+  printf "\n"
+  printf "PLATFORM\n"
+  printf "  Name of machine you are building on\n"
+  printf "\n"
+  printf "OPTIONS\n"
+  printf "  -c, --compiler=COMPILER\n"
+  printf "      compiler to use; valid options are 'intel(d)', 'gnu'\n"
+  printf "  -t, --components=\"COMPONENT1;COMPONENT2...\"\n"
+  printf "      components to include in model build, delimited with ';'\n"
+  printf "  -r, --remove_build\n"
+  printf "      remove existing build directory\n"
+  printf "\n"
+  printf "NOTE: This script is for internal developer use only;\n"
+  printf "See User's Guide for detailed build instructions\n"
+}
+
+PLATFORM=""
+COMPILER="intel"
+COMPONENTS=""
+RMBUILD=false
+
+if [ $# -lt 1 ]; then
+  echo "ERROR: not enough arguments"; usage; exit 1
+elif [[ $1 == "-"* ]]; then
+  echo "ERROR: missing arguments"; usage; exit 1
+fi
+PLATFORM="$1"; shift
+
+while :; do
+  case $1 in
+# HELP
+    --help|-h) usage; exit 0 ;;
+# PLATFORM
+    --platform|-p)
+      if [ "$2" ]; then
+        PLATFORM=$2; shift
+      else
+        echo "ERROR: $1 requires an argument."; usage; exit 1
+      fi
+      ;;
+    --platform=?*) PLATFORM=${1#*=} ;;
+    --platform=) echo "ERROR: $1 requires an argument."; usage; exit 1 ;;
+# COMPILER
+    --compiler|-c)
+      if [ "$2" ]; then
+        COMPILER=$2; shift
+      else
+        echo "ERROR: $1 requires an argument."; usage exit 1
+      fi
+      ;;
+    --compiler=?*) COMPILER=${1#*=} ;;
+    --compiler=) echo "ERROR: $1 requires an argument."; usage; exit 1 ;;
+# COMPONENTS
+    --components|-t)
+      if [ "$2" ]; then
+        COMPONENTS=$2; shift
+      else
+        echo "ERROR: $1 requires an argument."; usage; exit 1
+      fi
+      ;;
+    --components=?*) COMPONENTS=${1#*=} ;;
+    --components=)
+      echo "ERROR: $1 requires an argument."; usage; exit 1 ;;
+# REMOVE BUILD
+    --remove_build|-r) RMBUILD=true ;;
+# END
+    --)
+      shift; break ;;
+# UNKNOWN
+    -?*)
+      echo "ERROR: Unknown option $1"; usage; exit 1 ;;
+# DEFAULT
+    *)
+      break
+  esac
+  shift
+done
+
 set -eu
 
 #cd to location of script
 MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 
-usage () {
-  echo "Usage: "
-  echo "  $0 PLATFORM COMPILER"
-  echo ""
-  echo "PLATFORM: Name of machine you are building on"
-  echo "COMPILER: (optional) compiler to use; valid options are 'intel', 'gnu'"
-  echo ""
-  echo "NOTE: This script is for internal developer use only;"
-  echo "See User's Guide for detailed build instructions"
-}
-
-PLATFORM="${1:-NONE}"
-COMPILER="${2:-intel}"
-
-
-if [ $# -lt 1 ]; then 
-  echo "ERROR: not enough arguments"
-  usage
-  exit 1
-fi
-if [ $# -gt 2 ]; then
-  echo "ERROR: too many arguments"
-  usage
-  exit 1
-fi
-
-if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then 
-  usage
-  exit 0
-fi
-
 ENV_FILE="env/build_${PLATFORM}_${COMPILER}.env"
+
 if [ ! -f "$ENV_FILE" ]; then
   echo "ERROR: environment file ($ENV_FILE) does not exist for this platform/compiler combination"
   echo "PLATFORM=$PLATFORM"
@@ -47,6 +98,10 @@ fi
 
 # If build directory already exists, offer a choice
 BUILD_DIR=${MYDIR}/build
+
+if [ "${RMBUILD}" = true ] ; then
+  rm -rf ${BUILD_DIR}
+fi
 
 if [ -d "${BUILD_DIR}" ]; then
   while true; do
@@ -70,7 +125,11 @@ fi
 
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-cmake .. -DCMAKE_INSTALL_PREFIX=..
+if [ ! -z "$COMPONENTS" ]; then
+  cmake .. -DCMAKE_INSTALL_PREFIX=.. -DINCLUDE_COMPONENTS=${COMPONENTS}
+else
+  cmake .. -DCMAKE_INSTALL_PREFIX=..
+fi
 make -j ${BUILD_JOBS:-4}
 
 exit 0
